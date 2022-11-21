03a0c8b05f8542d0bac62f29083fed5bf16b6d76
Alvoid101 <10101010alvoid@gmail.com>
2022-11-21 15:06:13 -0500

    Agregar, borrar, graficar base

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import numpy as np
 from time import sleep
 
@@ -13,38 +14,45 @@ def clickAddButton():
     funtions = ""
     
     for i in fnlist:
-        funtions += "\n*"+i+"\n"
+        funtions += "\n➤ "+i+"\n"
     fnListLabel.config(text=funtions)
 
+def clickEraseButton():
+    plt.cla()
+    canvasFn.draw()
+    fnlist.clear()
+    fnListLabel.config(text="")
+
 def clickGraphButton():
-    image = tk.PhotoImage(file='seno.png')
-    graphic.create_image(320,240, image=image)
-    x = np.linspace(-10, 10, 300)
+    plt.cla()
     for i in fnlist:
+        x = np.linspace(-10, 10, 52)
         y = eval(i)
-        plt.plot(x, y)
-    plt.savefig('seno.png')
-    plt.show()
-    plt.close(fig='seno.png')
+        fn.plot(x, y)
+        canvasFn.draw()
+
+
 #--------Funtion Frame
 fxFrame = tk.Frame(root)
 funtionEntry = tk.Entry(fxFrame)
 fnTitleLabel = tk.Label(fxFrame,text="FUNCIONES", font=("Times New Roman", 13))
 fnListLabel = tk.Label(fxFrame)
-addButton = tk.Button(fxFrame,text="Agregar", command=clickAddButton)
-graphicButton = tk.Button(fxFrame,text="Gráficar", command=clickGraphButton)
+addButton = tk.Button(fxFrame,text="Agregar", command=clickAddButton, width=7)
+eraseButton = tk.Button(fxFrame, text="Borrar", command=clickEraseButton, width=7)
+graphicButton = tk.Button(fxFrame,text="Gráficar", command=clickGraphButton, width=7)
 
 funtionEntry.grid(row=0, column=0)
 fnTitleLabel.grid(row=1, column=0)
 fnListLabel.grid(row=2, column=0)
 addButton.grid(row=3, column=0)
-graphicButton.grid(row=4, column=0)
+eraseButton.grid(row=4, column=0)
+graphicButton.grid(row=5, column=0)
 #---------Graphic Frame
 grFrame = tk.Frame()
-image = tk.PhotoImage(file='seno.png')
-graphic = tk.Canvas(grFrame, width=640, height=480, bg="black")
-
-graphic.grid(row=0, column=0)
+fig, fn = plt.subplots(dpi=100, figsize=(5, 5), sharey=True, facecolor="#FFFFFF")
+canvasFn = FigureCanvasTkAgg(fig, master=grFrame)
+canvasFn.get_tk_widget().grid(row=0, column=0)
+canvasFn.draw()
 #--------Frames
 fxFrame.grid(row=1, column=0)
 grFrame.grid(row=1, column=1)
